77ce7ea98f996a2662c7c6cffc7509ead15ec6aa
Denis Gitelman <den.gitelman@gmail.com>
2024-11-30 14:58:05 +0300

    Реализованы тесты для исключений

---
+++ StringCalc.Tests/ErrorHandlingTests.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringCalc.Tests
+{
+    public class ErrorHandlingTests
+    {
+        [Fact]
+        public void Should_ThrowException_For_InvalidExpression()
+        {
+            var calculator = new StringCalculator();
+
+            Assert.Throws<InvalidOperationException>(() => calculator.Calculate("2 + + 3"));
+        }
+
+        [Fact]
+        public void Should_ThrowException_For_UnbalancedParentheses()
+        {
+            var calculator = new StringCalculator();
+
+            Assert.Throws<InvalidOperationException>(() => calculator.Calculate("(2 + 3 * 4"));
+        }
+        [Fact]
+        public void Should_ThrowException_For_DivisionByZero()
+        {
+            var calculator = new StringCalculator();
+
+            Assert.Throws<DivideByZeroException>(() => calculator.Calculate("10 / 0"));
+        }
+
+        [Fact]
+        public void Should_ThrowException_For_EmptyExpression()
+        {
+            var calculator = new StringCalculator();
+
+            Assert.Throws<InvalidOperationException>(() => calculator.Calculate(""));
+        }
+
+        [Fact]
+        public void Should_ThrowException_For_ExtraNumbers()
+        {
+            var calculator = new StringCalculator();
+
+            Assert.Throws<InvalidOperationException>(() => calculator.Calculate("2 3 + 4"));
+        }
+        [Fact]
+        public void Should_ThrowException_For_UnsupportedOperation()
+        {
+            var calculator = new StringCalculator();
+
+            Assert.Throws<InvalidOperationException>(() => calculator.Calculate("2 ^ 3"));
+        }
+
+    }
+}
